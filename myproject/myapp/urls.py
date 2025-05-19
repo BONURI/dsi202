f89e421eabb73188a9a5effc6d2548fd80343984
nutchaya <ritchaya.nuttirun@gmail.com>
2025-05-19 21:32:08 +0700

    คำนำ

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,4 +5,14 @@ from . import views
 urlpatterns = [
     path('', home, name='home'),  # Home page at root URL
     path('', views.home, name='home'),
+    
+   
 ]
+
+
+
+
+
+
+
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Tenant, Landlord, Room, Review, Booking, Admin
 from django.db.models import Q  # For complex queries
+
+from django.shortcuts import render, get_object_or_404
+from .models import Room
  
 # Home Page (FBV)
 def home(request):
     featured_rooms = [] 
     return render(request, 'home.html', {'featured_rooms': featured_rooms})
+
+
